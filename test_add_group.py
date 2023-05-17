f0c8b64dc213e92372558cea52f9cc58b7fac06f
Kuramych <vsewslod@yandex.ru>
2023-05-18 00:30:15 +0300

    first_test_scenario

--- test_add_group.py
+++ test_add_group.py
@@ -7,48 +7,69 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
-        self.wd.implicitly_wait(30)
-    
+        self.driver = webdriver.Chrome(executable_path=r'')
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
     def test_add_group(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("adfsfas")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("asfasfafs")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("asfasfas")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+        driver = self.driver
+        driver.get("http://localhost/addressbook/group.php")
+        driver.find_element("name","user").click()
+        driver.find_element("name","user").clear()
+        driver.find_element("name","user").send_keys("admin")
+        driver.find_element("name","pass").click()
+        driver.find_element("name","pass").clear()
+        driver.find_element("name","pass").send_keys("secret")
+        driver.find_element("xpath","//input[@value='Login']").click()
+        driver.find_element("link text","groups").click()
+        driver.find_element("name","new").click()
+        driver.find_element("name","group_name").click()
+        driver.find_element("name","group_name").clear()
+        driver.find_element("name","group_name").send_keys("adfsfas")
+        driver.find_element("name","group_header").click()
+        driver.find_element("name","group_header").clear()
+        driver.find_element("name","group_header").send_keys("asfasfafs")
+        driver.find_element("name","group_footer").click()
+        driver.find_element("name","group_footer").clear()
+        driver.find_element("name","group_footer").send_keys("asfasfas")
+        driver.find_element("name","submit").click()
+        driver.find_element("link text","group page").click()
+        driver.find_element("link text","Logout").click()
+
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
-    
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
     def tearDown(self):
-        self.wd.quit()
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
 
 
 if __name__ == "__main__":
