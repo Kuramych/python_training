d8581a222ba15d43a139889fc9f83440cd72440c
Kuramych <vsewslod@yandex.ru>
2023-05-19 21:38:46 +0300

    2.3 Created new packages for each file.py

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,6 @@
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 
 @pytest.fixture
@@ -9,10 +9,12 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
+
 def test_add_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="adfsfas", header="asfasfafs", footer="asfasfas"))
     app.logout()
+
 
 def test_add_empty_group(app):
     app.login(username="admin", password="secret")
